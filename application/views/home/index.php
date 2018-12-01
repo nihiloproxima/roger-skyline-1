579664e795d567c021affd24d2bb0cee249a75ec
Nathan Plouvier <naplouvi@student.le-101.fr>
2018-12-01 17:33:20 +0100

    update

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -10,7 +10,8 @@
 	<p>Partie optionnelle : application web</p><br />
 	<p>
 		Cette application toute simple permet de poster des articles avec un titre et un commebtaire. Ces articles sont enregistrés dans une base de données.
-		Il en va de même pour les utilisateurs et les commentaires rattachés aux articles.
+		Il en va de même pour les utilisateurs et les commentaires rattachés aux articles. 
+		Tables disponibles à <a href="192.168.56.3/phpmyadmin" target="blank">cette addresse</a>.
 	</p>
 	<a class="btn btn-primary btn-lg" href="<?php echo base_url('articles'); ?>" role="button">Accéder aux articles</a>
 </div>
