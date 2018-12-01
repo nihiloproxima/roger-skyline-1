c297f80a92b4104a047e2a723b023e6728e68c2b
Nathan Plouvier <naplouvi@student.le-101.fr>
2018-12-01 17:41:14 +0100

    update

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -10,8 +10,10 @@
 	<p>Partie optionnelle : application web</p><br />
 	<p>
 		Cette application toute simple permet de poster des articles avec un titre et un commebtaire. Ces articles sont enregistrés dans une base de données.
-		Il en va de même pour les utilisateurs et les commentaires rattachés aux articles. <br /><br />
-		Phpmyadmin disponible à <a href="https://192.168.56.3/phpmyadmin" target="blank">cette addresse</a>. Bonne visite.
+		Il en va de même pour les utilisateurs et les commentaires rattachés aux articles. <br />
+	</p>
+	<p>
+		Phpmyadmin disponible à <a href="https://192.168.56.3/phpmyadmin" target="blank">cette addresse</a>. Bonne visite.	<br />
 	</p>
 	<a class="btn btn-primary btn-lg" href="<?php echo base_url('articles'); ?>" role="button">Accéder aux articles</a>
 </div>
