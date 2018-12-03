78aa9734b68ea6ffa45b0feabf33576b7deab1da
Nathan Plouvier <naplouvi@student.le-101.fr>
2018-12-03 12:58:32 +0100

    test profil

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -2,6 +2,6 @@
 
     <h2 class="title"><?php echo $title ?></h2>
 
-    <img class="rounded col-md-2 offset-md-1" src="https://scontent-cdg2-1.xx.fbcdn.net/v/t1.0-1/p320x320/25348843_10213078215744301_10920749537487794_n.jpg?_nc_cat=100&_nc_ht=scontent-cdg2-1.xx&oh=2d29b98266dfb6a59e86f47c018466e6&oe=5C3CD0E0" alt="profil_pic"/>
-
+    <img class="rounded col-md-2 offset-md-1" src="https://www.qualiscare.com/wp-content/uploads/2017/08/default-user.png" alt="profil_pic"/>
+	<h4><?php echo $this->session->username ?></h4>
 </div>
