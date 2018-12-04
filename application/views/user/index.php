aff41c0df16d01175db884409f5f07d46de5f507
Nathan Plouvier <naplouvi@student.le-101.fr>
2018-12-04 12:26:49 +0100

    deploiement fonctionnel

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -2,7 +2,7 @@
 
     <h2 class="title"><?php echo $title ?></h2>
 
-    <img class="rounded col-md-2 offset-md-1" src="https://www.qualiscare.com/wp-content/uploads/2017/08/default-user.png" alt="profil_pic"/>
+    <img class="rounded col-md-2 offset-md-1" src="https://www.qualiscare.com/wp-content/uploads/2017/08/default-user.png" alt="profil_pic"/><br />
 	<h5 class=" col-md-2 offset-md-1 text-center"><?php echo $this->session->username ?></h5>
 </div>
 <br />
